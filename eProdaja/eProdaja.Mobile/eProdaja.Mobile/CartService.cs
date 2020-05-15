e8531ef373bea86980b84bf8bb55a8558f56fab4
Ajdin Ćatić <ajdin.catic@edu.fit.ba>
2020-05-15 16:56:14 +0200

    v10

---
+++ eProdaja/eProdaja.Mobile/eProdaja.Mobile/CartService.cs
@@ -0,0 +1,12 @@
+﻿using eProdaja.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eProdaja.Mobile
+{
+    public static class CartService
+    {
+        public static Dictionary<int, ProizvodDetailViewModel> Cart { get; set; } = new Dictionary<int, ProizvodDetailViewModel>();
+    }
+}
--- eProdaja/eProdaja.Mobile/eProdaja.Mobile/ViewModels/ProizvodiViewModel.cs
+++ eProdaja/eProdaja.Mobile/eProdaja.Mobile/ViewModels/ProizvodiViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using eProdaja.Model;
+using eProdaja.Model.Requests;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -9,24 +11,65 @@ using Xamarin.Forms;
 
 namespace eProdaja.Mobile.ViewModels
 {
-    public class ProizvodiViewModel
+    public class ProizvodiViewModel : BaseViewModel
     {
         private readonly APIService _proizvodiService = new APIService("Proizvod");
-        public ObservableCollection<Proizvod> ProizvodiList { get; set; } = new ObservableCollection<Proizvod>();
-
-        public ICommand InitCommand { get; set; }
+        private readonly APIService _vrsteProizvodaService = new APIService("VrsteProizvoda");
 
         public ProizvodiViewModel()
         {
             InitCommand = new Command(async () => await Init());
         }
 
+        public ObservableCollection<Proizvod> ProizvodiList { get; set; } = new ObservableCollection<Proizvod>();
+        public ObservableCollection<VrsteProizvoda> VrsteProizvodaList { get; set; } = new ObservableCollection<VrsteProizvoda>();
+
+        VrsteProizvoda _selectedVrstaProizvoda = null;
+        public VrsteProizvoda SelectedVrstaProizvoda
+        {
+            get { return _selectedVrstaProizvoda; }
+            set { 
+                SetProperty(ref _selectedVrstaProizvoda, value);
+                if (value != null)
+                    InitCommand.Execute(null);
+            }
+        }
+
+        public ICommand InitCommand { get; set; }
+
         public async Task Init()
         {
-            var list = await _proizvodiService.Get<IEnumerable<Proizvod>>(null);
+            if(VrsteProizvodaList.Count() == 0)
+            {
+                var listVrsteProizvodi = await _vrsteProizvodaService.Get<List<VrsteProizvoda>>(null);
+
+                foreach (var vrsteProizvoda in listVrsteProizvodi)
+                {
+                    VrsteProizvodaList.Add(vrsteProizvoda);
+                }
+            }
+
+            if(SelectedVrstaProizvoda != null)
+            {
+                ProizvodiSearchRequest request = new ProizvodiSearchRequest
+                {
+                    VrstaId = SelectedVrstaProizvoda.VrstaId
+                };
+
+                var listProizvodi = await _proizvodiService.Get<IEnumerable<Proizvod>>(request);
+
+                ProizvodiList.Clear();
+                foreach (var proizvod in listProizvodi)
+                {
+                    ProizvodiList.Add(proizvod);
+                }
+                return;
+            }
+
+            var listProizvodii = await _proizvodiService.Get<IEnumerable<Proizvod>>(null);
 
             ProizvodiList.Clear();
-            foreach (var proizvod in list)
+            foreach (var proizvod in listProizvodii)
             {
                 ProizvodiList.Add(proizvod);
             }
